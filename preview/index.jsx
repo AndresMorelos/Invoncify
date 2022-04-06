08ff3135b59b82abb897a3166168fa5d2277a9d0
Andres <andresmorelos123@gmail.com>
2022-04-06 10:36:28 -0500

    Increasing the percentage or request sent to performance monitoring.

--- preview/index.jsx
+++ preview/index.jsx
@@ -30,7 +30,7 @@ import '@styles/preview/print.css'
 Sentry.init({
   dsn: 'https://369beb9600244b6e83ef6f3fe77b4d29@o1191884.ingest.sentry.io/6313417',
   integrations: [new BrowserTracing()],
-  tracesSampleRate: 0.2
+  tracesSampleRate: 0.5
 });
 
 // Middleware
