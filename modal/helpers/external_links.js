2167d019d1b3ba3e362c769aa78283894600d2ca
Andres Morelos <andresmorelos123@gmail.com>
2022-06-19 20:37:45 -0500

    Showing button Open Release Page.

---
+++ modal/helpers/external_links.js
@@ -0,0 +1,25 @@
+import { shell } from '@electron/remote';
+
+const supportExternalLinks = (event) => {
+  let href;
+  let isExternal = false;
+
+  const checkDomElement = (element) => {
+    if (element.nodeName === 'A') {
+      href = element.getAttribute('href');
+    }
+    if (element.classList.contains('js-external-link')) {
+      isExternal = true;
+    }
+    if (href && isExternal) {
+      shell.openExternal(href);
+      event.preventDefault();
+    } else if (element.parentElement) {
+      checkDomElement(element.parentElement);
+    }
+  };
+
+  checkDomElement(event.target);
+};
+
+document.addEventListener('click', supportExternalLinks, false);
